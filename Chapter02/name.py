f875957a6f70281a9e63a646ae9417382df8f9cd
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-15 13:09:07 +0300

    concatenation

---
+++ Chapter02/name.py
@@ -0,0 +1,12 @@
+name = "ada lovelace"
+print(name.title())  # changing first letter to upper
+
+name = "Ada Lovelace"
+print(name.upper())  # changing all letter to upper
+print(name.lower())  # changing all letters to lower
+
+first_name = "ada"
+second_name = "lovelace"
+full_name = first_name + " " + second_name  # String concatenation
+print(full_name.title())
+print("Hello" + ", " + full_name.title() + "!")
