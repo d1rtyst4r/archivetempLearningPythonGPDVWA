4c31ca24ed23efd4abbd4e44b341f942d6b00e30
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-15 20:06:47 +0300

    changing element in the list

--- Chapter03/motorcycles.py
+++ Chapter03/motorcycles.py
@@ -3,3 +3,7 @@ print(motorcycles)
 
 motorcycles[0] = 'ducati'  # changing first element in list
 print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.append('ducati')  # add element in the end of the list
+print(motorcycles)
