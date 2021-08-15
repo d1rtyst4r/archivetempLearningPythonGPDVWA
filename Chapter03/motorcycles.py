c96f1ed373c13167f273c0544dd32f3807562a4d
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-15 20:16:59 +0300

    changing element in the list

--- Chapter03/motorcycles.py
+++ Chapter03/motorcycles.py
@@ -20,3 +20,18 @@ print(motorcycles)
 motorcycles = ['honda', 'yamaha', 'suzuki']
 motorcycles.insert(0, 'ducati')
 print(motorcycles)
+
+# Remove element using del
+motorcycles = ['honda', 'yamaha', 'suzuki']
+del motorcycles[0]
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+del motorcycles[1]
+print(motorcycles)
+
+# Remove element using pop
+motorcycles = ['honda', 'yamaha', 'suzuki']
+poped_motocycle = motorcycles.pop()  # remove last element
+print(motorcycles)
+print(poped_motocycle)
