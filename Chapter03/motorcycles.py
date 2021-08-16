c764846c0b5786118a6f9996bc6b02eebb24c7ce
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-16 19:34:55 +0300

    changing element in the list

--- Chapter03/motorcycles.py
+++ Chapter03/motorcycles.py
@@ -35,3 +35,23 @@ motorcycles = ['honda', 'yamaha', 'suzuki']
 poped_motocycle = motorcycles.pop()  # remove last element
 print(motorcycles)
 print(poped_motocycle)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+last_owned = motorcycles.pop()
+print("The last motorcycle I owned was a " + last_owned.title() + ".")
+
+# Remove element from list using index and pop()
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(0)
+print("The first motorcycle I owned was a " + first_owned.title() + ".")
+
+# Remove element from the list by value (remove())
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+motorcycles.remove('ducati')
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print("\nA " + too_expensive.title() + " is to expensive for me.")
