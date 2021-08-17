a71a174dae004c938bdc6218695ec4e9155bcd23
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-17 15:33:14 +0300

    simple statistics

---
+++ Chapter04/simple_statistics.py
@@ -0,0 +1,5 @@
+# Creating list with numbers from 0 to 9
+digits = list(range(0, 10))
+print(min(digits))  # return minimum
+print(max(digits))  # return maximum
+print(sum(digits))  # return sum of all numbers in list
