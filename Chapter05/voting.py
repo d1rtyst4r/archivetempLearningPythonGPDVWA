6e61c940fe192b4eab3eef9519f4f0478ad6c598
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-18 17:51:05 +0300

    if, if-else check

---
+++ Chapter05/voting.py
@@ -0,0 +1,14 @@
+# simple if check
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    print('Have you registered to vote yet?')
+
+# if-else check
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print('Have you registered to vote yet?')
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
