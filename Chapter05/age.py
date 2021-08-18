6bad26bb1739a48db897810f5b3e78be3048eea6
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-18 16:09:02 +0300

    IF, True and False, comparison of numbers

--- Chapter05/age.py
+++ Chapter05/age.py
@@ -11,5 +11,5 @@ print()
 # and/or
 age_01 = 22
 age_02 = 18
-print((age_01 >= 21) and (age_02 >= 21))  # and
-print((age_01 >= 21) or (age_02 >= 21))  # or
+print((age_01 >= 21) and (age_02 >= 21))  # and return True if bought arguments are True
+print((age_01 >= 21) or (age_02 >= 21))  # or return True if one of arguments is True
