4062b599b81bc827f79713536ff70585c1b8e5de
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-18 17:28:27 +0300

    Not in the list

---
+++ Chapter05/banned_users.py
@@ -0,0 +1,5 @@
+# Not in the list
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+if user not in banned_users:
+    print(user.title() + ", you can post a response if you wish.")
--- Chapter05/toppings.py
+++ Chapter05/toppings.py
@@ -2,3 +2,8 @@
 requested_topping = 'mushrooms'
 if requested_topping != 'anchovies':
     print('Hold the anchovies!')
+
+# Check is a element in the list
+requested_toppings = ['mushrooms', 'bacon', 'pepperoni']
+print('mushrooms' in requested_toppings)
+print('onion' in requested_toppings)
