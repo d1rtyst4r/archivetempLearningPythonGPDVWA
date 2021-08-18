7d0816571485dbf2966aadaae54aaca3e017dfd6
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-18 11:07:55 +0300

    task 4.11 list copy

--- Chapter04/tasks/pizzas.py
+++ Chapter04/tasks/pizzas.py
@@ -3,3 +3,16 @@ for pizza in pizzas:
     print('I like ' + pizza + 'pizza.')
 print('\nI really like pizza!')
 
+# Task 4.11
+my_pizzas = ['pepperoni', 'margarita', 'chicago']
+friend_pizzas = my_pizzas[:]  # list copy
+
+my_pizzas.append('vasara')
+friend_pizzas.append('bacon')
+print('\nMy favorite pizzas are:')
+for pizza in my_pizzas:
+    print('\t' + pizza.title())
+
+print('\nMy friend favorite pizzas are:')
+for pizza in friend_pizzas:
+    print('\t' + pizza.title())
