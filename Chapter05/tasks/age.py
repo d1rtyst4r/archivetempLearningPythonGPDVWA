201f9ef868c7d48dfb7d982757cea6a42e89d7aa
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-18 20:54:42 +0300

    task 5.6

---
+++ Chapter05/tasks/age.py
@@ -0,0 +1,13 @@
+age = 0
+if age < 2:
+    print('You are newborn.')
+elif age < 4:
+    print('You are baby.')
+elif age < 13:
+    print('You are child.')
+elif age < 20:
+    print('You are teenager.')
+elif age < 65:
+    print('Your are adult.')
+else:
+    print('You are senior.')
