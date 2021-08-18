728300f3b91e353fffab2482530ea7459e38b3ad
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-18 11:10:05 +0300

    task 4.12

--- Chapter04/tasks/foods.py
+++ Chapter04/tasks/foods.py
@@ -31,3 +31,17 @@ print('My favorite foods are:')
 print(my_foods)
 print('\nMy friend favorite foods are:')
 print(friend_foods)
+
+# Tas 4.12
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]  # list copy
+my_foods.append('ice cream')
+friend_foods.append('cannoli')
+
+print('\nMy favorite foods are:')
+for food in my_foods:
+    print('\t' + food.title())
+
+print('\nMy friend favorite foods are:')
+for food in friend_foods:
+    print('\t' + food.title())
