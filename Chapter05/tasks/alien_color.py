80bd9331a2abb95e94e3b86e16f8e6bb214cd305
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-18 20:35:09 +0300

    task 5.3-5.5

---
+++ Chapter05/tasks/alien_color.py
@@ -0,0 +1,57 @@
+alien_color = 'green'
+points = 0
+if alien_color == 'green':
+    points = 5
+print('You got ' + str(points) + ' points.')
+
+alien_color = 'yellow'
+points = 0
+if alien_color == 'green':
+    points = 5
+print('You got ' + str(points) + ' points.')
+
+alien_color = 'green'
+points = 0
+if alien_color == 'green':
+    points = 5
+else:
+    points = 10
+print('You got ' + str(points) + ' points.')
+
+alien_color = 'yellow'
+points = 0
+if alien_color == 'green':
+    points = 5
+else:
+    points = 10
+print('You got ' + str(points) + ' points.')
+
+alien_color = 'green'
+points = 0
+if alien_color == 'green':
+    points = 5
+elif alien_color == 'yellow':
+    points = 10
+else:
+    points = 15
+print('You got ' + str(points) + ' points.')
+
+alien_color = 'yellow'
+points = 0
+if alien_color == 'green':
+    points = 5
+elif alien_color == 'yellow':
+    points = 10
+else:
+    points = 15
+print('You got ' + str(points) + ' points.')
+
+alien_color = 'red'
+points = 0
+if alien_color == 'green':
+    points = 5
+elif alien_color == 'yellow':
+    points = 10
+else:
+    points = 15
+print('You got ' + str(points) + ' points.')
