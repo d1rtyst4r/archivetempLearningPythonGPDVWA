990c6f9011d1208eabd9bff4a5a237592840d10e
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-19 15:51:55 +0300

    task 5.7

---
+++ Chapter05/tasks/favorite_fruits.py
@@ -0,0 +1,8 @@
+# Check is element in the list
+favorite_fruits = ['apple', 'pineapple', 'orange']
+if 'apple' in favorite_fruits:
+    print("You really likes apples.")
+if 'pineapple' in favorite_fruits:
+    print("You really likes pineapples.")
+if 'orange' in favorite_fruits:
+    print("You really likes oranges.")
