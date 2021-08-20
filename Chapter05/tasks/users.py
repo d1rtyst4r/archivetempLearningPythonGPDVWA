72a050c10ce12d4a70c4fe7597f359b45c70d325
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-20 13:10:34 +0300

    task 5.9

--- Chapter05/tasks/users.py
+++ Chapter05/tasks/users.py
@@ -1,9 +1,12 @@
 # Special rule for 'admin' task 5.8
-users= ['eric', 'alice', 'alex', 'admin']
+users = ['eric', 'alice', 'alex', 'admin']
 
-for user in users:
-    if user == 'admin':
-        print('Hello ' + user + ', would you like to see a status report?')
-    else:
-        print('Hello ' + user.title() + ', thank you for logging again.')
+if users:
+    for user in users:
+        if user == 'admin':
+            print('Hello ' + user + ', would you like to see a status report?')
+        else:
+            print('Hello ' + user.title() + ', thank you for logging again.')
+else:
+    print('We need to find some users!')
 
