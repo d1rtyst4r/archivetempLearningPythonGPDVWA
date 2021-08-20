9a8cee2065cd816b45cb610f21bff4901dc07c54
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-20 11:11:11 +0300

    check element from one list in another

--- Chapter05/toppings.py
+++ Chapter05/toppings.py
@@ -36,3 +36,14 @@ if requested_toppings:  # return true if any element is in the list
     print("\nFinished making your pizza.")
 else:
     print('Are you sure you want plain pizza?')
+
+# Check element from 1 list in another
+available_toppings = ['mushrooms', 'olives', 'green pepper', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'fresh fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print('Adding ' + requested_topping + '.')
+    else:
+        print("Sorry, we don't have " + requested_topping + '.')
+print('\nFinished making your pizza.')
