3f011ed788ac6df00e7b7d74fe98d39342dd0590
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-21 13:44:48 +0300

    Add new data to dictionary

---
+++ Chapter06/alien.py
@@ -0,0 +1,13 @@
+# Dictionaries
+
+alien_0 = {'color': 'green', 'points': 5}  # Key : Value
+print(alien_0['color'])
+print(alien_0['points'])
+
+new_points = alien_0['points']
+print("You just eared " + str(new_points) + " points!")
+
+# Add new data to dictionary
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
