1d621258d099b6ec77b8925b46e5903dd86727c3
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-29 20:04:03 +0300

    flags

---
+++ Chapter07/counting.py
@@ -0,0 +1,6 @@
+# cycle while
+
+current_number = 1
+while current_number <= 5:  # while TRUE!!!
+    print(current_number)
+    current_number += 1
--- Chapter07/parrot.py
+++ Chapter07/parrot.py
@@ -1,4 +1,13 @@
-# input()
+# input() and flags
 
-message = input("Tell me something and I will repeat it back to you: ")
-print(message)
+prompt = "\nTell me something and I will repeat it back to you."
+prompt += "\nEnter 'quit' to end the program. "
+message = ''
+
+active = True
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
