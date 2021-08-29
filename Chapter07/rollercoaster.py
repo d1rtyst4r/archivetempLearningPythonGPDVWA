a6e2e43e06ba0a695a7f857c646510073eb40354
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-29 14:26:48 +0300

    method int()

---
+++ Chapter07/rollercoaster.py
@@ -0,0 +1,7 @@
+# method int()
+height = input("How tall are you, in inches :")
+height = int(height)
+if height >= 36:
+    print("\nYou are tall enough to right.")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
