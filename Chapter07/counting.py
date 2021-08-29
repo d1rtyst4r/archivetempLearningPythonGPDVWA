b29bc6dc38d134f9ef42061d192a8f1221c8786f
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-29 20:24:49 +0300

    continue

--- Chapter07/counting.py
+++ Chapter07/counting.py
@@ -4,3 +4,11 @@ current_number = 1
 while current_number <= 5:  # while TRUE!!!
     print(current_number)
     current_number += 1
+
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue  # skip next part of cycle and return to the begin of cycle
+    print(current_number)
+
