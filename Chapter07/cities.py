30ac36eb79e88d8835619197b4bfb7c5eab34762
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2021-08-29 20:19:09 +0300

    break

---
+++ Chapter07/cities.py
@@ -0,0 +1,11 @@
+# break
+
+prompt = "\nPlease enter the name of city you have visited:"
+prompt += "\nEnter 'quit' when you are finished!"
+
+while True:
+    city = input(prompt)
+    if city == 'quit':
+        break
+    else:
+        print("I'd love to go to " + city.title() + "!")
