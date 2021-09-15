a1b9ee6f8af3302c4578b52765da0af463c8b39d
d1rtyst4r <d1rtyst4r@gmail.com>
2021-09-15 17:21:35 +0300

    formatted name

--- Chapter08/formatted_name.py
+++ Chapter08/formatted_name.py
@@ -1,8 +1,13 @@
-def get_formatted_name(first_name, last_name):
+def get_formatted_name(first_name, last_name, middle_name=''):
     """return formatted full name"""
-    full_name = first_name.title() + " " + last_name.title()
+    if middle_name:
+        full_name = first_name.title() + " " + middle_name.title() + " " + last_name.title()
+    else:
+        full_name = first_name.title() + " " + last_name.title()
     return full_name
 
 
 musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+musician = get_formatted_name('john', 'hooker', 'lee')
 print(musician)
