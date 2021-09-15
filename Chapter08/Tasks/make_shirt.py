0c4cb9f2a6fd86f019aa919f901a3cf5b2ea2994
d1rtyst4r <d1rtyst4r@gmail.com>
2021-09-15 16:25:57 +0300

    task 8-4

--- Chapter08/Tasks/make_shirt.py
+++ Chapter08/Tasks/make_shirt.py
@@ -1,7 +1,8 @@
-def make_shirt(size, text):
+def make_shirt(size='L', text='I love Python'):
     """simple function return string with t-shirt size and text on it"""
     print('Your shirt size is ' + size + " and text " + text + ".")
 
 
 make_shirt('xl', "test")
 make_shirt(size='L', text="piu piu")
+make_shirt()
