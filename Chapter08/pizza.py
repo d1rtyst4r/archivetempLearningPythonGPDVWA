f59056bcfb97e6647ebe8912062ce68073df818e
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2023-04-01 13:29:42 +0300

    updated pizza

--- Chapter08/pizza.py
+++ Chapter08/pizza.py
@@ -1,6 +1,8 @@
 def make_pizza(*toppings):
     """Print ordered topppings"""
-    print(toppings)
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
 
 
 make_pizza('pepperoni')
