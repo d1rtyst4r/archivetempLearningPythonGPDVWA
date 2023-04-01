075e90e4463d359f982eb5f827d4e83095c456c3
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2023-04-01 13:20:05 +0300

    task 8-9 and 8-10

---
+++ Chapter08/Tasks/make_great.py
@@ -0,0 +1,15 @@
+def show_magicians(magicians):
+    for magician in magicians:
+        print(magician.title() + ".")
+
+
+def make_great(magicians):
+    for i in range(len(magicians)):
+        magicians[i] = "Greate " + magicians[i]
+
+        
+
+magicians_names = ['david', 'agonisyan', 'blane']
+show_magicians(magicians_names)
+make_great(magicians_names)
+show_magicians(magicians_names)
---
+++ Chapter08/Tasks/show_magican.py
@@ -0,0 +1,8 @@
+def show_magicians(magicians):
+    for magician in magicians:
+        print("Magican name: " + magician.title() + ".")
+    
+
+
+magicians_names = ['david', 'agonisyan', 'blane']
+show_magicians(magicians_names)
