c2100d57f46d9b0c753890a27b93f03618b5ba16
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2023-04-02 12:56:17 +0300

    pizza

--- Chapter08/pizza.py
+++ Chapter08/pizza.py
@@ -1,9 +1,9 @@
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """Print ordered topppings"""
-    print("\nMaking a pizza with the following toppings:")
+    print("\nMaking a + " + str(size) + "inch pizza with the following toppings:")
     for topping in toppings:
         print("- " + topping)
 
 
-make_pizza('pepperoni')
-make_pizza('pepperoni', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'pepperoni', 'green peppers', 'extra cheese')
