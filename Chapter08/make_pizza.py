76a56b676811d9c686f79490e348625bc66ecd6e
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2023-04-02 13:36:36 +0300

    module rename

--- Chapter08/make_pizza.py
+++ Chapter08/make_pizza.py
@@ -1,4 +1,4 @@
-from pizza import make_pizza as mp
+import pizza as p
 
-mp(16, 'pepperoni')
-mp(12, 'pepperoni', 'green peppers', 'extra cheese')
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'pepperoni', 'green peppers', 'extra cheese')
