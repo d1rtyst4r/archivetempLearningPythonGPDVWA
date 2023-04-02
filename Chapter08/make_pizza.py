794cab59c3f2cfabb677d7add5f3ecf3daf71e44
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2023-04-02 13:38:04 +0300

    import all functions

--- Chapter08/make_pizza.py
+++ Chapter08/make_pizza.py
@@ -1,4 +1,4 @@
-import pizza as p
+from pizza import *
 
-p.make_pizza(16, 'pepperoni')
-p.make_pizza(12, 'pepperoni', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'pepperoni', 'green peppers', 'extra cheese')
